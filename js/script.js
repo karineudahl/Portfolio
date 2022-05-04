bebbc5f73858c94f2178f6e400962255fd294f65
karineudahl <74554925+karineudahl@users.noreply.github.com>
2022-05-04 12:48:35 +0200

    up

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const cursor = document.querySelector(".cursor"); 
-
-document.addEventListener("mousemove", (e) => {
-    let x = e.clientX; 
-    let y = e.clientY; 
-
-    cursor.style.top = y + "px"; 
-    cursor.style.left = x + "px"; 
-});
